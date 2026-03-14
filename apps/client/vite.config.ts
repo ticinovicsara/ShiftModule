bb482ce1ac028ac871a930686d8aa8286279dda0
ticinovicsara <ticinovicsara0@gmail.com>
2026-03-15 00:27:40 +0100

    chore(packages): add @repo/types exports field and path aliases

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path/win32";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@repo/types": path.resolve(
+        __dirname,
+        "../../packages/types/src/index.ts",
+      ),
+    },
+  },
 });
